a4968e01514eb578395c89f965196215f04b9d4f
Joe <ngwujoseph90@gmail.com>
2025-08-20 12:56:20 +0100

    Meta

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Homepage from './pages/Homepage'
 import Connect  from './pages/connect'
-import Meta from "./pages/Meta"
 
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/connect" element={<Connect />} />
-        <Route path="/Meta" element={<Meta />} />
       </Routes>
     </Router>
       </>
